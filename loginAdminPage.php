0c037b8f8923a3840cafb8153224a7f0cc403d6a
rangashin <99878706+rangashin@users.noreply.github.com>
2022-07-18 00:44:00 +0800

    Deleted Login Admin Page and New link for index

--- loginAdminPage.php
+++ loginAdminPage.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="loginAdmin.css">
+    <link rel="stylesheet" href="loginAdminPage.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 </head>
