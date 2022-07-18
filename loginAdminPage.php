8e1851b875917fbd4805458b414cba4d164dbd3d
rangashin <99878706+rangashin@users.noreply.github.com>
2022-07-18 15:09:15 +0800

    Update loginAdminPage.php

--- loginAdminPage.php
+++ loginAdminPage.php
@@ -5,9 +5,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="assets/img/180.png" rel="icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>PhilNance - Admin Page</title>
     <link rel="stylesheet" href="loginAdminPage.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
